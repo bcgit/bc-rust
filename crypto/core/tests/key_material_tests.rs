4f053af7a62429877c8e33777223c17136916f41
laruizlo <luis.bvesp@gmail.com>
2026-06-22 10:43:54 -0400

    style: fix formatting issues

--- crypto/core/tests/key_material_tests.rs
+++ crypto/core/tests/key_material_tests.rs
@@ -188,9 +188,9 @@ mod test_key_material {
         assert_eq!(key.key_type(), KeyType::Zeroized);
 
         // zeroize() must wipe the entire backing buffer.
-        // Full capacity must be inspected to confirm the previously-set bytes were 
+        // Full capacity must be inspected to confirm the previously-set bytes were
         // actually overwritten with zeros.
-        // Note: key_len is now 0, so ref_to_bytes() returns an empty slice. 
+        // Note: key_len is now 0, so ref_to_bytes() returns an empty slice.
         key.allow_hazardous_operations();
         let full_buf = key.mut_ref_to_bytes().unwrap();
         assert_eq!(full_buf.len(), capacity);
@@ -296,7 +296,7 @@ mod test_key_material {
             Ok(()) => { /* good */ }
             _ => panic!("Expected Ok(())"),
         }
-        
+
         // each KeyType can convert to itself
 
         let mut key = KeyMaterial256::from_bytes(&DUMMY_KEY[..32]).unwrap();
@@ -702,12 +702,12 @@ mod test_key_material {
 
     #[test]
     fn eq() {
-        // For context: 
+        // For context:
         // DUMMY_KEY: &[u8; 64] = b"\x00\x01\x02\x03\x04\x05\x06\x07\x08\x09\x0A\x0B\x0C\x0D\x0E\x0F\
         //                           \x10\x11\x12\x13\x14\x15\x16\x17\x18\x19\x1A\x1B\x1C\x1D\x1E\x1F\
         //                           \x20\x21\x22\x23\x24\x25\x26\x27\x28\x29\x2A\x2B\x2C\x2D\x2E\x2F\
         //                           \x30\x31\x32\x33\x34\x35\x36\x37\x38\x39\x3A\x3B\x3C\x3D\x3E\x3F";
-        
+
         // Same bytes, full capacity. Should be equal.
         let key1 = KeyMaterial256::from_bytes(&DUMMY_KEY[..32]).unwrap();
         let key2 = KeyMaterial256::from_bytes(&DUMMY_KEY[..32]).unwrap();
@@ -722,13 +722,19 @@ mod test_key_material {
         assert_ne!(key1, key_short);
 
         // PartialEq ignores key_type: same bytes, different KeyType. Should be equal.
-        let key_low = KeyMaterial256::from_bytes_as_type(&DUMMY_KEY[..32], KeyType::BytesLowEntropy).unwrap();
-        let key_mac = KeyMaterial256::from_bytes_as_type(&DUMMY_KEY[..32], KeyType::MACKey).unwrap();
+        let key_low =
+            KeyMaterial256::from_bytes_as_type(&DUMMY_KEY[..32], KeyType::BytesLowEntropy).unwrap();
+        let key_mac =
+            KeyMaterial256::from_bytes_as_type(&DUMMY_KEY[..32], KeyType::MACKey).unwrap();
         assert_eq!(key_low, key_mac);
 
         // PartialEq ignores security_strength: same bytes, different strength. Should be equal.
-        let key_strong = KeyMaterial256::from_bytes_as_type(&DUMMY_KEY[..32], KeyType::BytesFullEntropy).unwrap();
-        let mut key_weak = KeyMaterial256::from_bytes_as_type(&DUMMY_KEY[..32], KeyType::BytesFullEntropy).unwrap();
+        let key_strong =
+            KeyMaterial256::from_bytes_as_type(&DUMMY_KEY[..32], KeyType::BytesFullEntropy)
+                .unwrap();
+        let mut key_weak =
+            KeyMaterial256::from_bytes_as_type(&DUMMY_KEY[..32], KeyType::BytesFullEntropy)
+                .unwrap();
         key_weak.set_security_strength(SecurityStrength::_128bit).unwrap();
         assert_ne!(key_strong.security_strength(), key_weak.security_strength()); // strengths differ
         assert_eq!(key_strong, key_weak); // but keys are still equal
